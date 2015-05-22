7a2271b2a9fa515349908e19be78f847154012ea
Jeff Vance <jvance@redhat.com>
2015-05-22 16:10:54 -0700

    brkmnt and brkdev parsing

--- newvol.sh
+++ newvol.sh
@@ -13,7 +13,7 @@
 function parse_cmd() {
 
   local long_opts='replica:,kube-master:,volname:'
-  local rand_cmd; local first
+  local first
 
   eval set -- "$(getopt -o ' ' --long $long_opts -- $@)"
 
@@ -45,7 +45,6 @@ function parse_cmd() {
     # generate a random volname, starting with a random uppercase letter
     first=$(($RANDOM % 26 + 65)) # 65..90
     VOLNAME=$(printf \\$(printf '%03o' $first)) # poor man's chr()
-    ##rand_cmd="cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1"
     VOLNAME+="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1)"
     echo "INFO: volume name omitted, using random name \"$VOLNAME\""
   fi
@@ -63,8 +62,72 @@ function parse_cmd() {
   return 0
 }
 
+# parse_nodes_brkmnts_blkdevs: set the global NODES array, and the NODE_BRKMNTS
+# and NODE_BLKDEVS assoc arrays, based on NODE_SPEC. The format of the assoc
+# arrays is:
+#   NODE_BRKMNTS[<node>]="<brickmnt>[ <brkmnt1>][ <brmknt2>]..."
+#   NODE_BLKDEVS[<node>]="<blkdev>[ <blkdev>][ <blkdev>]..."
+# The brick mount and block dev values are a lists. Most times the list
+# contains only one brick-mnt/block-dev, but to handle the case of the same
+# node repeated with different brick-mnt and/or block-dev paths we use a list.
+# Returns 1 on errors.
+#
+function parse_nodes_brkmnts_blkdevs() {
+
+  local node_spec=(${NODE_SPEC[0]//:/ }) # split after subst ":" with space
+  local def_brkmnt=${node_spec[1]} # default
+  local def_blkdev=${node_spec[2]} # default
+  local brkmnts=(); local blkdev; local brkmnt
+
+  if [[ -z "$def_brkmnt" || -z "$def_blkdev" ]] ; then
+    echo "ERROR: expect a brick mount and block device to immediately follow the first node (each separated by a \":\")"
+    return 1
+  fi
+
+  # remove trailing / if present in default brkmnt
+  def_brkmnt="${def_brkmnt%/}"
+
+  # parse out list of nodes, format: "node[:brick-mnt][:blk-dev]"
+  for node_spec in ${NODE_SPEC[@]}; do
+      node=${node_spec%%:*}
+      # fill in missing brk-mnts and/or blk-devs
+      case "$(grep -o ':' <<<"$node_spec" | wc -l)" in # num of ":"s
+          0) # brkmnt and blkdev omitted
+             NODE_BRKMNTS[$node]+="$def_brkmnt "
+             NODE_BLKDEVS[$node]+="$def_blkdev "
+          ;;
+          1) # only brkmnt specified
+             brkmnt="${node_spec#*:}"; brkmnt="${brkmnt%/}"; # no trailing /
+             NODE_BRKMNTS[$node]+="$brkmnt "
+             NODE_BLKDEVS[$node]+="$def_blkdev "
+          ;;
+          2) # either both brkmnt and blkdev specified, or just blkdev specified
+             blkdev="${node_spec##*:}"
+             NODE_BLKDEVS[$node]+="${blkdev} "
+             brkmnts=(${node_spec//:/ }) # array
+             if [[ "${brkmnts[1]}" == "$blkdev" ]] ; then # "::", empty brkmnt
+               NODE_BRKMNTS[$node]+="$def_brkmnt "
+             else
+               brkmnt="${brkmnts[1]}"; brkmnt="${brkmnt%/}"; # no trailing /
+               NODE_BRKMNTS[$node]+="$brkmnt "
+             fi
+          ;;
+          *)
+             echo "ERROR: improperly specified node-spec list"
+             return 1
+          ;;
+      esac
+  done
+
+  # assign unique storage nodes
+  NODES=($(printf '%s\n' "${!NODE_BRKMNTS[@]}" | sort))
+
+  return 0
+}
 
 ## main ##
+
+declare -A NODE_IPS; declare -A NODE_BRKMNTS; declare -A NODE_BLKDEVS
 
 parse_cmd $@ || exit -1
 
